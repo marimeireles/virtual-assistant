d99f67231bbe88d4e0ea354d9cfed05d82f0cc21
Mariana Meireles <mariana@psychonautgirl.space>
2019-10-31 11:40:18 +0100

    final adjustments on logging and general cleanup

--- dialog.py
+++ dialog.py
@@ -5,6 +5,7 @@ import io
 import json
 import torch
 import numpy as np
+import logging
 from collections import OrderedDict
 
 from TTS.models.tacotron import Tacotron
@@ -21,9 +22,9 @@ from PySide2.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel
 from PySide2.QtCore import Signal, QUrl, Qt
 from PySide2.QtMultimedia import QSound
 
-MODEL_PATH = './tts_model/best_model.pth.tar'
-CONFIG_PATH = './tts_model/config.json'
-OUT_FILE = 'tts_out.wav'
+MODEL_PATH = "./tts_model/best_model.pth.tar"
+CONFIG_PATH = "./tts_model/config.json"
+OUT_FILE = "tts_out.wav"
 CONFIG = load_config(CONFIG_PATH)
 use_cuda = False
 
@@ -42,23 +43,24 @@ class Dialog(QWidget):
     def process_user_message(self):
         ''' Shows user's message in screen and send it to the ChatBot '''
         self.send_user_msg_to_chatbot(self.userText)
+        logging.debug("User message: {self.textResponse}")
 
     def set_user_message(self, string):
         self.userText = string
 
     def send_user_msg_to_chatbot(self, message):
         self.sqlConversationModel.send_message("machine", message, "Me")
-        headers = {'Content-type': 'application/json'}
+        headers = {"Content-type": "application/json"}
         data = "{\"sender\": \"user1\", \"message\": \" " + message + "\"}"
-        self.response = requests.post('http://localhost:5005/webhooks/rest/webhook', headers=headers, data=data)
+        self.response = requests.post("http://localhost:5005/webhooks/rest/webhook", headers=headers, data=data)
 
     def process_machine_message(self):
         '''Shows machine's message and reproduce its voice'''
         self.textResponse = json.loads(self.response.text)[0]["text"]
-        print("Machine: " + self.textResponse)
         self.sqlConversationModel.send_message("Me", self.textResponse, "machine")
 
         self.tts.tts_predict(self.model, MODEL_PATH, self.textResponse, CONFIG, use_cuda, self.ap, OUT_FILE)
+        logging.debug("Machine message: {self.textResponse}")
         QSound.play(OUT_FILE);
 
 class TTS():
@@ -70,13 +72,13 @@ class TTS():
         return alignment, spectrogram, stop_tokens, wav_norm
 
     def load_tts_model(self):
-        MODEL_PATH = './tts_model/best_model.pth.tar'
-        CONFIG_PATH = './tts_model/config.json'
+        MODEL_PATH = "./tts_model/best_model.pth.tar"
+        CONFIG_PATH = "./tts_model/config.json"
         CONFIG = load_config(CONFIG_PATH)
         use_cuda = False
 
         num_chars = len(phonemes) if CONFIG.use_phonemes else len(symbols)
-        model = Tacotron(num_chars, CONFIG.embedding_size, CONFIG.audio['num_freq'], CONFIG.audio['num_mels'], CONFIG.r, attn_windowing=False)
+        model = Tacotron(num_chars, CONFIG.embedding_size, CONFIG.audio["num_freq"], CONFIG.audio["num_mels"], CONFIG.r, attn_windowing=False)
 
         # load the audio processor
         # CONFIG.audio["power"] = 1.3
@@ -90,7 +92,7 @@ class TTS():
             cp = torch.load(MODEL_PATH, map_location=lambda storage, loc: storage)
 
         # load the model
-        model.load_state_dict(cp['model'])
+        model.load_state_dict(cp["model"])
         if use_cuda:
             model.cuda()
 
