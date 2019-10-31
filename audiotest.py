50f1e737bcff50e7d5bc9cfb43bae7f929fd895b
Mariana Meireles <mariana@psychonautgirl.space>
2019-10-31 11:49:32 +0100

    Cleaning repo

--- audiotest.py
+++
@@ -1,8 +0,0 @@
-from PySide2.QtMultimedia import QMediaPlayer
-from PySide2.QtCore import QObject, Signal, Slot, QUrl
-
-player = QMediaPlayer()
-player.setMedia(QUrl.fromLocalFile("tts_out.wav"))
-player.setVolume(50);
-player.play()
-
--- main.qml
+++
@@ -1,8 +0,0 @@
-import QtQuick 2.12
-import QtQuick.Window 2.12
-Window {
-    title: qsTr("Hello World")
-    width: 640
-    height: 480
-    visible: true
-}
